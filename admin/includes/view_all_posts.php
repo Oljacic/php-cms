dfc633a0a7e089f4dea9a23db922ad64fc4bed7f
Stefan Oljacic <djedpool@gmail.com>
2020-01-03 10:50:30 +0100

    Handling way of update post.

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -12,7 +12,8 @@
             <th>Tags</th>
             <th>Comment Count</th>
             <th>Date</th>
-            <th>Actions</th>
+            <th>Edit</th>
+            <th>Delete</th>
         </tr>
         </thead>
         <tbody>
@@ -37,17 +38,27 @@
                 $table.= "<td>{$post_id}</td>";
                 $table.= "<td>{$post_author}</td>";
                 $table.= "<td>{$post_title}</td>";
-                $table.= "<td width='550'>{$post_content}</td>";
-                $table.= "<td>{$post_category_id}</td>";
+                $table.= "<td>{$post_content}</td>";
+
+                $query = "SELECT * FROM categories ";
+                $query.= "WHERE id = {$post_category_id}";
+
+                $select_categories_id = mysqli_query($connection, $query);
+
+                while ($row = mysqli_fetch_assoc($select_categories_id)) {
+                    $category_id = $row['id'];
+                    $category_title = $row['category_title'];
+
+                    $table.= "<td>{$category_title}</td>";
+                }
+
                 $table.= "<td>{$post_status}</td>";
                 $table.= "<td><img width='100' src='../images/{$post_image}'></td>";
                 $table.= "<td>{$post_tags}</td>";
                 $table.= "<td>{$post_comment_count}</td>";
                 $table.= "<td>{$post_date}</td>";
-                $table.= "<td>
-                            <a href='posts.php?delete_post={$post_id}'>Delete</a> ---
-                            <a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a>
-                          </td>";
+                $table.= "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
+                $table.= "<td><a href='posts.php?delete_post={$post_id}'>Delete</a></td>";
                 $table.= "</tr>";
 
                 echo $table;

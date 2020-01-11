f53319a3d8ae1686ef63c361cfcd0340fcab6c3d
Stefan Oljacic <djedpool@gmail.com>
2020-01-11 12:11:03 +0100

    Working with status for posts.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <div class="col-md-8">
             <h1 class="page-header">Page Heading<small>Secondary Text</small></h1>
             <?php
-                $query = "SELECT * FROM posts";
+                $query = "SELECT * FROM posts ";
                 $select_all_posts = mysqli_query($connection, $query);
 
                 while ($row = mysqli_fetch_assoc($select_all_posts)) :
@@ -19,18 +19,20 @@
                     $post_date = $row['date'];
                     $post_image = $row['image'];
                     $post_content = substr($row['content'], 0, 100);
-
+                    $post_status = $row['status'];
             ?>
-                 <!-- First Blog Post -->
-                <h2><a href="post.php?post_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a></h2>
-                <p class="lead">by <a href="index.php"><?php echo $post_author; ?></a></p>
-                <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date; // Posted on August 28, 2013 at 10:00 PM ?></p>
-                <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-                <hr>
-                <p><?php echo $post_content; ?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-                <hr>
+                <?php if($post_status == 'published') : ?>
+                    <!-- First Blog Post -->
+                    <h2><a href="post.php?post_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a></h2>
+                    <p class="lead">by <a href="index.php"><?php echo $post_author; ?></a></p>
+                    <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date; // Posted on August 28, 2013 at 10:00 PM ?></p>
+                    <hr>
+                    <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                    <hr>
+                    <p><?php echo $post_content; ?></p>
+                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                    <hr>
+                <?php endif; ?>
             <?php endwhile; ?>
         </div>
         <!-- Blog Sidebar Widgets Column -->

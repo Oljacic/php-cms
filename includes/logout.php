a44c756595f3c80aca3a7a0c24339ccdf5a2d497
Stef94 <oljacici@gmail.com>
2020-01-13 15:52:29 +0100

    Working on login and logout functionality.

---
+++ includes/logout.php
@@ -0,0 +1,11 @@
+<?php 
+
+session_start();
+
+$_SESSION['user_id'] = null;
+$_SESSION['username'] = null;
+$_SESSION['firstname'] = null;
+$_SESSION['lastname'] = null;
+$_SESSION['role'] = null;
+
+header('Location: ../index.php');
